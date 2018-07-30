4ff1d542155d931dde4320a6e568591ce3792be9
Cody Blakeney <cjb92@txstate.edu>
2018-07-30 00:05:55 -0500

    inital top 25 analysis

--- team_ranking_data_mine.py
+++ team_ranking_data_mine.py
@@ -5,13 +5,27 @@ Created on Fri Jul 20 16:17:29 2018
 
 @author: cody
 """
-
+#	
+# {
+# 	"2017" : {
+# 		"Alabama" : 1,
+# 		"Texas" : 2
+# 	}
+# }
+#
+#
 from bs4 import BeautifulSoup
 import glob
 import re
 
-for file in glob.glob("/home/cody/football_analysis/*.html"):
-    with open(file, 'rb') as page:
-        soup = BeautifulSoup(page)
-        for stuff in soup.find_all("span", {"class": "tltp_arw_tp"}):
-            print(stuff)
+years = {}
+for file in glob.glob("/Users/codyblakeney/football_analysis/*.html"):
+	years[file] = {}
+	with open(file, 'rb') as page:
+		soup = BeautifulSoup(page)
+		for stuff in soup.find_all(name="div", attrs={"class" : "name"}):
+			for other in stuff.findChildren():
+				print(stuff, '\n' , other.get_text())
+				years[file][other.get_text()] = 1
+
+		print(years[file])
